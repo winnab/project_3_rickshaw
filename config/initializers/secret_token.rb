e331ae02237cae86cf264a365a3ee9fa08570c2e
winnab <winnab@gmail.com>
2013-12-11 14:02:23 +0000

    created rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project3Rickshaw::Application.config.secret_token = '17abea793a4ec0a4b9bdcda53c7eb713331338f9bbabf3066007373e1a8cf51d3e698c54ea3488d89a0072fa42d88c19093e7cb8e7cbdecaf29f9a720db88e61'
